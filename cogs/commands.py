aeb5d2dd9999341e9bef359191ef3556d6d481cb
watermelonTm <searcheac@gmail.com>
2021-01-13 16:23:32 +0100

    Update commands.py

--- cogs/commands.py
+++ cogs/commands.py
@@ -333,6 +333,10 @@ class Moderation(commands.Cog):
             sanity_limit = 135
             await channel.trigger_typing()
             await asyncio.sleep(1)
+        except TypeError:
+            await channel.send(embed = discord.Embed(
+                description = "Inserisci un limite della sanity valido",
+                colour = discord.Colour.purple()))
         except IndexError:
             sanity_limit = 130
 
